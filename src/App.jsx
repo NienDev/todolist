d42fd26854dd53e9269595cbab46c3dfd1af4d78
NienDev <niewebdev@gmail.com>
2022-11-21 22:25:58 +0700

    dragging function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useReducer, useContext, useEffect } from "react";
+import { useState, useReducer, useContext, useEffect, useRef } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -19,45 +19,58 @@ import { set, ref, onValue, remove, update } from "firebase/database";
 
 function TodoItem(props) {
   return (
-    <div>
-      <div className="flex justify-between items-center p-4 shadow-md">
-        {props.check ? (
-          <div className="text-lg font-semibold text-gray-300 line-through">
-            {props.content}
-          </div>
-        ) : (
-          <div className="text-lg font-semibold text-[green]">
-            {props.content}
-          </div>
-        )}
-
-        <div className="flex items-center gap-2 ">
-          <button onClick={() => props.handleCheck(props.id)}>
-            {props.check ? (
-              <img
-                src="/todolist/x.svg"
-                alt="x-icon"
-                className="h-[24px] w-[24px]"
-              />
-            ) : (
-              <img
-                src="/todolist/tick.svg"
-                alt="tick-icon"
-                className="h-[24px] w-[24px]"
-              />
-            )}
-          </button>
-          <button>
-            <img
-              src="/todolist/bin.svg"
-              alt="bin-icon"
-              className="h-[24px] w-[24px]"
-              onClick={() => {
-                props.handleDelete(props.id);
-              }}
-            />
-          </button>
+    <div
+      className="flex justify-between items-center p-4 shadow-md"
+      draggable
+      onDragStart={() => {
+        props.dragItem.current = props.order;
+      }}
+      onDragEnter={() => {
+        props.dragOverItem.current = props.order;
+      }}
+      onDragEnd={() => {
+        props.handleDragging();
+      }}
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+    >
+      {props.check ? (
+        <div className="text-lg font-semibold text-gray-300 line-through">
+          {props.content}
         </div>
+      ) : (
+        <div className="text-lg font-semibold text-[green]">
+          {props.content}
+        </div>
+      )}
+
+      <div className="flex items-center gap-2 ">
+        <button onClick={() => props.handleCheck(props.id)}>
+          {props.check ? (
+            <img
+              src="/todolist/x.svg"
+              alt="x-icon"
+              className="h-[24px] w-[24px]"
+            />
+          ) : (
+            <img
+              src="/todolist/tick.svg"
+              alt="tick-icon"
+              className="h-[24px] w-[24px]"
+            />
+          )}
+        </button>
+        <button>
+          <img
+            src="/todolist/bin.svg"
+            alt="bin-icon"
+            className="h-[24px] w-[24px]"
+            onClick={() => {
+              props.handleDelete(props.id);
+            }}
+          />
+        </button>
       </div>
     </div>
   );
@@ -69,6 +82,7 @@ const ACTIONS = {
   CHECK: "check",
   DELETEALL: "delete-all",
   INIT: "init",
+  SWAP: "swap",
 };
 
 function reducer(todos, action) {
@@ -96,6 +110,17 @@ function reducer(todos, action) {
       return [];
     case ACTIONS.INIT:
       return [...todos, action.payload.todoObj];
+    case ACTIONS.SWAP:
+      const uidDragItem = action.payload.dragItemObj.uidd;
+      const uidDragOverItem = action.payload.dragOverItemObj.uidd;
+      return todos.map((todo, i) =>
+        i === action.payload.dragItem.current
+          ? { ...action.payload.dragOverItemObj, uidd: uidDragItem }
+          : i === action.payload.dragOverItem.current
+          ? { ...action.payload.dragItemObj, uidd: uidDragOverItem }
+          : todo
+      );
+
     default:
       return todos;
   }
@@ -105,6 +130,8 @@ function TodoApp() {
   const [todos, dispatch] = useReducer(reducer, []);
   const [newTask, setNewTask] = useState("");
   const navigate = useNavigate();
+  const dragItem = useRef(0);
+  const dragOverItem = useRef(0);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -129,6 +156,21 @@ function TodoApp() {
     update(ref(db, `/${auth.currentUser.uid}/${checkID}`), {
       ...newTodo,
       done: !done,
+    });
+  }
+
+  function handleDragging() {
+    const dragItemObj = todos[dragItem.current];
+    const dragOverItemObj = todos[dragOverItem.current];
+    console.log(dragItemObj, dragOverItemObj);
+    dispatch({
+      type: ACTIONS.SWAP,
+      payload: {
+        dragItem,
+        dragOverItem,
+        dragItemObj,
+        dragOverItemObj,
+      },
     });
   }
 
@@ -160,6 +202,13 @@ function TodoApp() {
     });
   }, []);
 
+  useEffect(() => {
+    todos.forEach((todo) => {
+      set(ref(db, `${auth.currentUser.uid}/${todo.uidd}`), { ...todo });
+      console.log(todo);
+    });
+  }, [todos]);
+
   const writeToDatabase = (uidd) => {
     set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
       uidd: uidd,
@@ -189,7 +238,7 @@ function TodoApp() {
         />
       </form>
       <div>
-        {todos.map((task) => (
+        {todos.map((task, i) => (
           <TodoItem
             key={task.uidd}
             content={task.content}
@@ -197,6 +246,10 @@ function TodoApp() {
             handleCheck={handleCheck}
             id={task.uidd}
             check={task.done}
+            order={i}
+            handleDragging={handleDragging}
+            dragItem={dragItem}
+            dragOverItem={dragOverItem}
           />
         ))}
       </div>
@@ -341,7 +394,7 @@ function LoginPage() {
         </div>
         <div>
           Does not have account ?{" "}
-          <NavLink to="/todolist/signup" className="text-[blue] font-semibold">
+          <NavLink to="/signup" className="text-[blue] font-semibold">
             Sign Up here
           </NavLink>
         </div>
@@ -378,3 +431,5 @@ export default App;
 
 // drag item to reorder
 // when edit old note, it will become the latest note and will be at first
+
+// can not fix the err when in todo app reload the page and it will get err in git page
